97be09f9d16867085ea41f17a1c0880ab039e60d
miirsery <miirsery.main@gmail.com>
2025-10-22 13:46:41 +0700

    PGP-12: Настройка проекта

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,21 +3,21 @@
 import { fileURLToPath } from 'node:url'
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image'],
-  components: {
-    global: false,
-    dirs: [],
-  },
-  alias: {
-    '@': fileURLToPath(new URL('./app', import.meta.url)),
-    '#shared': fileURLToPath(new URL('./app/shared', import.meta.url)),
-    '#entities': fileURLToPath(new URL('./app/entities', import.meta.url)),
-    '#features': fileURLToPath(new URL('./app/features', import.meta.url)),
-    '#widgets': fileURLToPath(new URL('./app/widgets', import.meta.url)),
-    '#pages': fileURLToPath(new URL('./app/pages', import.meta.url)),
-    '#fonts': fileURLToPath(new URL('./app/assets/fonts', import.meta.url)),
-    styles: fileURLToPath(new URL('./app/assets/styles', import.meta.url)),
-  },
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+	modules: ['@nuxt/eslint', '@nuxt/image', '@nuxtjs/stylelint-module'],
+	components: {
+		global: false,
+		dirs: [],
+	},
+	alias: {
+		'@': fileURLToPath(new URL('./app', import.meta.url)),
+		'#shared': fileURLToPath(new URL('./app/shared', import.meta.url)),
+		'#entities': fileURLToPath(new URL('./app/entities', import.meta.url)),
+		'#features': fileURLToPath(new URL('./app/features', import.meta.url)),
+		'#widgets': fileURLToPath(new URL('./app/widgets', import.meta.url)),
+		'#pages': fileURLToPath(new URL('./app/pages', import.meta.url)),
+		'#fonts': fileURLToPath(new URL('./app/assets/fonts', import.meta.url)),
+		styles: fileURLToPath(new URL('./app/assets/styles', import.meta.url)),
+	},
 })
