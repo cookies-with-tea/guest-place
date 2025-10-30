b5e6ada08093ed3eca97499402c4652e4eb15b88
miirsery <miirsery.main@gmail.com>
2025-10-30 20:56:32 +0700

    PGP-15: Настройка проекта

--- apps/admin-translations/src/app/index.ts
+++ apps/admin-translations/src/app/index.ts
@@ -2,6 +2,8 @@ import App from './App.vue'
 import { router } from './router'
 import { createApp } from 'vue'
 
+import '@admin-panel/ui/styles/index.scss'
+
 const app = createApp(App)
 
 app.use(router)
