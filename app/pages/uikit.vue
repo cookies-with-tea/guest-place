5b56cf0fca8bf1a48ca59a9cecc94e919c6fea4d
Andrey  Kozloff <andrew08552@gmail.com>
2025-11-11 14:41:37 +0300

    PGP-46: отобразил компонент иконки на странице uikit*a

--- app/pages/uikit.vue
+++ app/pages/uikit.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="ui-kit">
-    <UiIcon style="color: #000" name="clear" width="50px" height="50px" />
+    <div class="box">
+      <div class="column">
+        <UiButton> Hi </UiButton>
 
-    <UiIcon style="color: #fff" name="eye-off" width="50px" height="50px" />
+        <UiButton> Hi </UiButton>
+
+        <UiButton> Hi </UiButton>
+      </div>
+
+      <div class="column">
+        <UiButton> Hi 2 </UiButton>
+
+        <UiButton> Hi 2 </UiButton>
+
+        <UiButton> Hi 2 </UiButton>
+      </div>
+    </div>
+
+    <div class="box">
+      <UiIcon style="color: #000" name="close" width="50px" height="50px" />
+
+      <UiIcon style="color: #f90" name="user" width="50px" height="50px" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { UiIcon } from '../shared/ui'
+import { UiButton, UiIcon } from '../shared/ui'
 </script>
 
 <style lang="scss" scoped>
+.ui-kit {
+  padding: 20px;
+}
+
 .box {
+  width: fit-content;
   display: flex;
   border: 1px dashed rgb(89 0 131);
   padding: 20px;
