2efb6c9895c2785d2b4351835cb18ad2e52fcde3
Alex <57665901+miirsery@users.noreply.github.com>
2025-11-12 11:22:05 +0700

    PGP-82: Настройка линтера в dev режиме (#12)

--- app/pages/index/index.vue
+++ app/pages/index/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>Index page</h1>
+  <div class="home-page">
+    <div class="test"></div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.test {
+  width: 40px;
+  height: 40px;
+  background-color: #f90;
+}
+</style>
