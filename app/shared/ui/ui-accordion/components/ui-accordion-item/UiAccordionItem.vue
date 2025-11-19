52ed074ddad490b14c6d813173fe7f3ed9f52dd6
Andrey  Kozloff <andrew08552@gmail.com>
2025-11-20 02:31:35 +0300

    PGP-96: сделал анимацию появление/скрытия текста

--- app/shared/ui/ui-accordion/components/ui-accordion-item/UiAccordionItem.vue
+++ app/shared/ui/ui-accordion/components/ui-accordion-item/UiAccordionItem.vue
@@ -9,8 +9,8 @@
         <UiIcon ref="plusIconRef" name="accordion-plus-minus" width="58px" height="58px" />
       </button>
     </div>
-
-    <div v-if="isActive(nameToString)" class="ui-accordion-item__content">
+    <!--    v-if="isActive(nameToString)"-->
+    <div class="ui-accordion-item__content" :class="{ 'ui-accordion-item__content_show': isActive(nameToString) }">
       <slot />
     </div>
   </li>
@@ -124,7 +124,6 @@ const toggleAccordion = () => {
   box-shadow: 0 4px 15px 0 #694e4b24;
   background-color: var(--color-white);
   padding: 13px 30px;
-  gap: 20px;
 
   &__trigger {
     @include typography(h5);
@@ -143,7 +142,24 @@ const toggleAccordion = () => {
   &__content {
     @include typography(h5);
 
+    max-height: 0;
     color: var(--color-text-light);
+    transform: translateY(-10px);
+    transition:
+      max-height 0.6s cubic-bezier(0.25, 0.46, 0.45, 0.94),
+      opacity 0.4s ease,
+      transform 0.4s ease;
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+    opacity: 0;
+
+    &_show {
+      max-height: 500px;
+      transform: translateY(0);
+      padding-top: 20px;
+      opacity: 1;
+    }
   }
 }
 </style>
