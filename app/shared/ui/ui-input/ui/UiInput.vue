b185a2367309f5e06af01866fd9198d8897ec698
Andrey  Kozloff <andrew08552@gmail.com>
2025-11-27 16:40:05 +0300

    PGP-62: cделал кейс size

--- app/shared/ui/ui-input/ui/UiInput.vue
+++ app/shared/ui/ui-input/ui/UiInput.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="ui-input" :class="classes">
-    <input :id="id" v-model="model" class="ui-input__inner" :placeholder="placeholder" />
+    <div v-if="$slots['prefix-icon'] || props.prefixIcon" class="ui-input__icon prefix-icon">
+      <slot name="prefix-icon">
+        <UiIcon :name="props.prefixIcon" />
+      </slot>
+    </div>
+
+    <input :id="id" v-model="model" class="ui-input__inner" :placeholder="props.placeholder" />
+
+    <div v-if="$slots['suffix-icon'] || props.suffixIcon" class="ui-input__icon suffix-icon">
+      <slot name="suffix-icon">
+        <UiIcon :name="props.suffixIcon" />
+      </slot>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { UiIcon } from '#shared/ui'
 import { computed, useId } from 'vue'
+// TODO: сделать кейс валидации
 
 interface IProps {
   placeholder: string
   showPassword: boolean
   type?: 'text' | 'password' | 'textarea' | 'number' | 'email'
-  suffixIcon?: boolean
+  suffixIcon?: string
+  prefixIcon?: string
   disabled?: boolean
   size?: 's' | 'md'
   rows?: number
@@ -19,12 +34,13 @@ interface IProps {
 
 const props = withDefaults(defineProps<IProps>(), {
   type: 'text',
-  suffixIcon: false,
   showPassword: false,
   disabled: false,
   size: 's',
   rows: 4,
 })
+
+console.log(props.prefixIcon)
 
 const model = defineModel()
 const id = useId()
@@ -37,6 +53,8 @@ const classes = computed(() => `ui-input--${props.size}`)
   //TODO: сделать глобально. повторяется во втором компоненте!
   --ui-input-primary-shadow-color: 0 4px 15px 0 #694e4b24;
   --ui-input-primary-border-color: transparent;
+  --ui-input-prefix-icon-color: var(--color-text-light);
+  --ui-input-suffix-icon-color: var(--color-text-light);
 
   width: 100%;
   display: flex;
@@ -47,16 +65,35 @@ const classes = computed(() => `ui-input--${props.size}`)
   background-color: var(--color-white);
   transition: border-color var(--transition-duration-primary) ease;
 
-  &__inner {
-    @include typography(body);
+  &__icon {
+    height: 100%;
+    display: flex;
+    flex-shrink: 0;
+    align-items: center;
+    justify-content: center;
+    margin-left: 8px;
 
+    :deep(.ui-icon) {
+      font-size: 24px;
+    }
+  }
+
+  .prefix-icon:deep(.ui-icon) {
+    color: var(--ui-input-prefix-icon-color);
+    margin-right: 8px;
+  }
+
+  .suffix-icon:deep(.ui-icon) {
+    color: var(--ui-input-suffix-icon-color);
+    margin-left: 8px;
+  }
+
+  &__inner {
     width: 100%;
     height: 100%;
     color: var(--color-text-dark);
 
     &::placeholder {
-      @include typography(body);
-
       color: var(--color-text-light);
     }
   }
@@ -67,18 +104,17 @@ const classes = computed(() => `ui-input--${props.size}`)
 
   &--md {
     height: 62px;
-
-    .ui-input__inner {
-      padding: 0 40px;
-    }
+    padding: 0 40px;
   }
 
   &--s {
     height: 38px;
+    padding: 0 25px;
+  }
 
-    .ui-input__inner {
-      padding: 0 25px;
-    }
+  &__inner,
+  &__inner::placeholder {
+    @include typography(body);
   }
 }
 </style>
