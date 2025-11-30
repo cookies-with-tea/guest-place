4fd545536622f69ee69d63545ac55eded930dbf7
Andrey  Kozloff <andrew08552@gmail.com>
2025-11-30 05:19:34 +0300

    PGP-62: починил глазик

--- app/shared/ui/ui-input/composables/useAnimateIcon.ts
+++ app/shared/ui/ui-input/composables/useAnimateIcon.ts
@@ -2,7 +2,6 @@ import { gsap } from 'gsap'
 import { MorphSVGPlugin } from 'gsap/MorphSVGPlugin'
 import { ScrambleTextPlugin } from 'gsap/ScrambleTextPlugin'
 import { onMounted, ref } from 'vue'
-import {useCheckElement} from './useCheckElement'
 
 gsap.registerPlugin(MorphSVGPlugin, ScrambleTextPlugin)
 
@@ -11,49 +10,46 @@ const TOGGLE_SPEED = 0.125
 const ENCRYPT_SPEED = 1
 const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~,.<>?/;":][}{+_)(*&^%$#@!±=-§'
 
-const startBlinking = () => {
-  if (blinkTimeline.value) blinkTimeline.value.kill()
-  const delay = gsap.utils.random(2, 8)
-  const repeat = Math.random() > 0.5 ? 3 : 1
+const checkItemExists = (iconEye) => {
+  const iconContainer = iconEye.value?.$el
+  if (!iconContainer) return
 
-  const {eyeOpen, eyeClosed, upper, lower} = useCheckElement()
+  const eyeOpen = iconContainer.querySelector('#eye-open path')
+  const eyeClosed = iconContainer.querySelector('#eye-closed path')
+  const eye = iconContainer.getElementById('eye')
+  const upper = iconContainer.getElementById('lid--upper')
+  const lower = iconContainer.getElementById('lid--lower')
 
-  blinkTimeline.value = gsap
-    .timeline({
-      delay,
-      onComplete: startBlinking,
-      repeat,
-      yoyo: true,
-    })
-    .to(upper, { morphSVG: lower, duration: BLINK_SPEED }, 0)
-    .to(eyeOpen, { morphSVG: eyeClosed, duration: BLINK_SPEED }, 0)
+  return {iconContainer, eyeOpen, eyeClosed, eye, upper, lower}
 }
 
-const moveEye = (e) => {
-  const resetEyeTimer = ref(null)
-  const {iconContainer, eye } = useCheckElement()
-
-  if (resetEyeTimer) resetEyeTimer.kill()
-
-  resetEyeTimer.value = gsap.delayedCall(2, () => {
-    gsap.to(eye, { xPercent: 0, yPercent: 0, duration: 0.2 })
-  })
-
-  const bounds = iconContainer.getBoundingClientRect()
-  const xPercent = gsap.utils.clamp(-30, 30, gsap.utils.mapRange(-100, 100, 30, -30)(bounds.x - e.clientX))
-  const yPercent = gsap.utils.clamp(-30, 30, gsap.utils.mapRange(-100, 100, 30, -30)(bounds.y - e.clientY))
-
-  gsap.set(eye, { xPercent, yPercent })
-}
-
-export const useAnimateIcon = () => {
+export const useAnimateIcon = (iconEye) => {
   const passwordInput = ref(null)
   // const passwordValue = ref('')
   const isPasswordVisible = ref(false)
   const isAnimating = ref(false)
   const blinkTimeline = ref(null)
+  const resetEyeTimer = ref(null)
 
-  const { eyeOpen, eyeClosed, upper, lower} = useCheckElement()
+
+  const { eyeOpen, eyeClosed, upper, lower, eye, iconContainer} = checkItemExists(iconEye)
+  console.log(iconContainer)
+  const startBlinking = () => {
+    if (blinkTimeline.value) blinkTimeline.value.kill()
+
+    const delay = gsap.utils.random(2, 8)
+    const repeat = Math.random() > 0.5 ? 3 : 1
+
+    blinkTimeline.value = gsap
+      .timeline({
+        delay,
+        onComplete: startBlinking,
+        repeat,
+        yoyo: true,
+      })
+      .to(upper, { morphSVG: lower, duration: BLINK_SPEED }, 0)
+      .to(eyeOpen, { morphSVG: eyeClosed, duration: BLINK_SPEED }, 0)
+  }
 
   const togglePassword = async () => {
     if (isAnimating.value) return
@@ -61,9 +57,6 @@ export const useAnimateIcon = () => {
 
     const currentValue = passwordValue.value
     const wasPassword = !isPasswordVisible.value
-
-
-
 
     if (wasPassword) {
       // Закрываем глаз
@@ -121,12 +114,31 @@ export const useAnimateIcon = () => {
     isAnimating.value = false
   }
 
-  onMounted(() => {
-    startBlinking()
-    window.addEventListener('pointermove', moveEye)
-  })
+  const moveEye = (e) => {
+    if (resetEyeTimer.value) resetEyeTimer.value.kill()
 
-  return { togglePassword}
+    resetEyeTimer.value = gsap.delayedCall(2, () => {
+      gsap.to(eye, { xPercent: 0, yPercent: 0, duration: 0.2 })
+    })
+
+    const bounds = iconContainer.getBoundingClientRect()
+    const xPercent = gsap.utils.clamp(-30, 30, gsap.utils.mapRange(-100, 100, 30, -30)(bounds.x - e.clientX))
+    const yPercent = gsap.utils.clamp(-30, 30, gsap.utils.mapRange(-100, 100, 30, -30)(bounds.y - e.clientY))
+
+    gsap.set(eye, { xPercent, yPercent })
+  }
+
+  // onMounted(() => {
+  //   startBlinking()
+  //   window.addEventListener('pointermove', moveEye)
+  // })
+  // onMounted(() => {
+    // useAnimateIcon(iconEye.value?.$el)
+    // startBlinking()
+    // window.addEventListener('pointermove', moveEye)
+  // })
+
+  return { moveEye, startBlinking }
 }
 
 
