3ccfe4d6a7795a2a65c8016bf9ade2f714a98f9e
Andrey  Kozloff <andrew08552@gmail.com>
2025-12-16 17:43:36 +0300

    PGP-62: add

--- app/shared/ui/ui-icon/types/index.ts
+++ app/shared/ui/ui-icon/types/index.ts
@@ -1,3 +1,5 @@
-/* prettier-ignore */
-// @ts-nocheck
-export type IconNamesType = 'accordion-plus-minus' | 'attach' | 'bell' | 'calendar-check' | 'calendar-default' | 'chat' | 'clock' | 'close-eye' | 'close' | 'coffe' | 'exit' | 'export' | 'favourites' | 'file' | 'filter' | 'house' | 'Identification-badge' | 'key' | 'like' | 'link' | 'location' | 'megaphone' | 'monitor-play' | 'open-eye' | 'pencil' | 'question' | 'search' | 'selection' | 'settings' | 'shop-car' | 'trash' | 'user'
+import type UiIcon from '../ui/UiIcon.vue'
+
+export * from './types'
+
+export type UiIconInstanceType = InstanceType<typeof UiIcon> | null
