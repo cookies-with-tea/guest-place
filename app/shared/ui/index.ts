30789d3ce9d7e5e59164ac3adb737da092dacd7e
Andrew <82395841+Andrew6556@users.noreply.github.com>
2025-12-21 10:14:23 +0300

    PGP-124: баги с кнопкой (#23)
    * client: pull
    
    * PGP-124: add
    
    * PGP-124: фикс бага с hover иконок
    
    * PGP-124: фикс бага 'z-index фона перебивает фон кнопок'
    
    ---------
    
    Co-authored-by: Andrey  Kozloff <andrew08552@gmail.com>

--- app/shared/ui/index.ts
+++ app/shared/ui/index.ts
@@ -5,3 +5,5 @@ export { default as UiIcon } from './ui-icon'
 export { default as UiAccordion } from './ui-accordion'
 
 export { default as UiAccordionItem } from './ui-accordion/components/ui-accordion-item'
+
+export { default as UiInput } from './ui-input'
