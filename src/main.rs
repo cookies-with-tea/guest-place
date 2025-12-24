41f5bd6bded8ca8dde72041bf6be4d3eb11b1b7b
miirsery <miirsery.main@gmail.com>
2025-12-24 23:38:07 +0700

    fix routing

--- src/main.rs
+++ src/main.rs
@@ -157,14 +157,14 @@ async fn main() {
 
     let public_router = Router::new()
         .nest("/api/v1/auth", auth::handlers::routing())
-        .nest("/api/v1/user", user::handlers::public_routing())
-        .nest("/api/v1/i18n", i18n::handlers::public_routing())
+        .nest("/api/v1/user", user::handlers::public_router())
+        .nest("/api/v1/i18n", i18n::handlers::public_router())
         .nest_service("/media", ServeDir::new("media"))
         .with_state(shared_state.clone());
 
     let protected_router = Router::new()
-        .nest("/api/v1/user", user::handlers::protected_routing())
-        .nest("/api/v1/i18n", i18n::handlers::protected_routing())
+        .nest("/api/v1/user", user::handlers::protected_router())
+        .nest("/api/v1/i18n", i18n::handlers::protected_router())
         .nest("/api/v1/media", media::handlers::routing())
         .with_state(shared_state.clone())
         .layer(middleware::from_fn_with_state(
--- src/user/handlers.rs
+++ src/user/handlers.rs
@@ -354,7 +354,7 @@ pub fn public_router() -> Router<Arc<AppState>> {
     Router::new().route("/", post(create))
 }
 
-pub fn private_router() -> Router<Arc<AppState>> {
+pub fn protected_router() -> Router<Arc<AppState>> {
     Router::new()
         .route("/", get(get_all))
         .route("/{id}", get(get_one).delete(delete_one))
