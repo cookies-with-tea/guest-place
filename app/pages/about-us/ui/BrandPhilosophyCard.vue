36c8caada71d3ab3c834923775d6de906d2cc642
miirsery <miirsery.main@gmail.com>
2026-02-24 20:17:55 +0700

    PGP-145: Исправление верстки

--- app/pages/about-us/ui/BrandPhilosophyCard.vue
+++ app/pages/about-us/ui/BrandPhilosophyCard.vue
@@ -1,40 +1,56 @@
 <template>
   <div class="card">
     <div class="card__content">
-      <div class="card__title">
-        наша <span>Миссия</span>
-      </div>
+      <div class="card__title"> наша <span>Миссия</span> </div>
 
       <div class="card__description">
         Соединяем гостей (людей) и места, создавая простоту и прозрачность в “отношениях”. Предоставляем самые
-        современные технологии и инструменты для простого и легкого общения.
-        Наши эксперты помогают Гостям найти <nuxt-link to="/">«то самое»</nuxt-link>место.
+        современные технологии и инструменты для простого и легкого общения. Наши эксперты помогают Гостям найти
+        <nuxt-link to="/">«то самое»</nuxt-link>место.
       </div>
     </div>
 
     <div class="card__img">
-      <img
-        src="https://cdn-imgix.headout.com/media/images/c9db3cea62133b6a6bb70597326b4a34-388-dubai-img-worlds-of-adventure-tickets-01.jpg?auto=format&w=1222.3999999999999&h=687.6&q=90&ar=16%3A9&crop=faces&fit=crop">
+      <img src="https://i.pinimg.com/originals/8f/57/f7/8f57f7e33c00c2c120ad00a5765a90db.jpg" />
     </div>
-
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped lang="scss">
+@use 'styles/helpers/mixins/responsive' as *;
+
 .card {
   display: flex;
   justify-content: space-between;
+  flex-direction: column;
   align-items: center;
   gap: 70px;
   color: var(--color-text-dark);
 
+  @include responsive-min(md) {
+    flex-direction: row;
+  }
+
   &__img {
-    max-height: 285px;
-    max-width: 475px;
+    height: 173px;
+    width: 100%;
+
+    @include responsive-min(sm) {
+      @include fluid-value(height, sm, md, 173px, 240px);
+    }
+
+    @include responsive-min(md) {
+      @include fluid-value(height, md, xxl, 240px, 285px);
+      @include fluid-value(max-width, md, xxl, 370px, 475px);
+    }
+
+    @include responsive-min(xxl) {
+      height: 285px;
+      max-width: 475px;
+      width: 100%;
+    }
 
     img {
       object-fit: cover;
