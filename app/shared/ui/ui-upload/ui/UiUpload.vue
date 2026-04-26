c870e1fa3c207fdd7d97ed7b5a76bfe908153866
Andrey  Kozloff <andrew08552@gmail.com>
2026-04-26 20:03:02 +0300

    PGP-213: базовая стилизация

--- app/shared/ui/ui-upload/ui/UiUpload.vue
+++ app/shared/ui/ui-upload/ui/UiUpload.vue
@@ -15,8 +15,8 @@
       </span>
 
       <input
-        type="file"
         id="avatar"
+        type="file"
         hidden
         name="avatar"
         accept="image/png, image/jpeg"
@@ -48,24 +48,23 @@ import { UiIcon } from '#shared/ui';
 @use 'public/styles/helpers/functions' as *;
 
 .ui-upload {
-  --ui-upload-border-color: #C6C6CC;
+  --ui-upload-border-color: #c6c6cc;
   --ui-upload-button-bg-color: var(--color-accent);
   --ui-upload-button-color: #fff;
 
-  display: flex;
   width: 100%;
-  gap: 8px;
+  height: 204px;
+  display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   border-radius: 4px;
-
   background-image: repeating-linear-gradient(179deg, var(--ui-upload-border-color), var(--ui-upload-border-color) 8px, transparent 8px, transparent 17px, var(--ui-upload-border-color) 17px), repeating-linear-gradient(269deg, var(--ui-upload-border-color), var(--ui-upload-border-color) 8px, transparent 8px, transparent 17px, var(--ui-upload-border-color) 17px), repeating-linear-gradient(-1deg, var(--ui-upload-border-color), var(--ui-upload-border-color) 8px, transparent 8px, transparent 17px, var(--ui-upload-border-color) 17px), repeating-linear-gradient(89deg, var(--ui-upload-border-color), var(--ui-upload-border-color) 8px, transparent 8px, transparent 17px, var(--ui-upload-border-color) 17px);
-  background-size: 2px calc(100% + 17px), calc(100% + 17px) 2px, 2px calc(100% + 17px) , calc(100% + 17px) 2px;
   background-position: 0 0, 0 0, 100% 0, 0 100%;
+  background-size: 2px calc(100% + 17px), calc(100% + 17px) 2px, 2px calc(100% + 17px) , calc(100% + 17px) 2px;
   background-repeat: no-repeat;
   animation: borderAnimation 0.9s infinite linear reverse;
-  height: 204px;
+  gap: 8px;
 
 
   &__on-drag {
@@ -77,8 +76,8 @@ import { UiIcon } from '#shared/ui';
   &__or {
     @include typography(h5);
 
-    color: var(--color-text-regular);
     font-weight: 300;
+    color: var(--color-text-regular);
   }
 
   &__btn {
@@ -86,11 +85,11 @@ import { UiIcon } from '#shared/ui';
 
     display: flex;
     align-items: center;
-    gap: 8px;
+    border-radius: 4px;
     color: var(--ui-upload-button-color);
     background-color: var(--ui-upload-button-bg-color);
-    border-radius: 4px;
     padding: 6px 12px;
+    gap: 8px;
 
     @include hover {
       color: darken(var(--ui-upload-button-color));
@@ -100,8 +99,8 @@ import { UiIcon } from '#shared/ui';
   &__max-weight {
     @include typography(body);
 
-    color: var(--color-text-regular);
     vertical-align: middle;
+    color: var(--color-text-regular);
   }
 }
 
